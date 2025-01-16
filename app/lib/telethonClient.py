18cbc92f17af6b87840ab7415959ca4d8a7bcde0
Max <maxjapri@gmail.com>
2025-01-16 16:50:05 +0700

    download newly sent file

--- app/lib/telethonClient.py
+++ app/lib/telethonClient.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
 load_dotenv()
@@ -10,4 +11,17 @@ client = TelegramClient(
 )
 @client.on(events.NewMessage)
 async def new_message_listener(event):
-  print(f"New message received: {event.text}")
+  message = event.message
+  messageid = message.id
+  chatid = ""
+  try:
+    chatid = message.peer_id.channel_id
+  except:
+    chatid = message.peer_id.user_id
+
+  if message.media:
+    start = datetime.datetime.now()
+    print('downloading...',)
+    await message.download_media(file=f'./downloads/{chatid}-{messageid}-{message.media.document.attributes[0].file_name}')
+    span = datetime.datetime.now() - start
+    print(f'done in {span}')
