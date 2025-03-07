51c0dc47a7e38fce43c8a271621a270d20f4d9c1
Max <maxjapri@gmail.com>
2025-03-07 15:09:16 +0700

    implement conversion code + database

--- app/telebot/events/newMessage.ts
+++ app/telebot/events/newMessage.ts
@@ -1,25 +1,42 @@
 import fs from 'fs';
 import { TelegramClient } from "telegram";
 import { parseFile } from '../../services/parsingService';
+import { getAllCredentials, insertCredentials } from '../../services/credentialService';
 
 const downloadsDirectory = './.downloads';
 
 export default {
     event: TelegramClient.events.NewMessage,
     execute: async function (event: any) {
-        let { message } = event;
-        let { peerId, id } = message;
-        let { channelId, userId } = peerId
+        const { message } = event;
+        const { peerId, id } = message;
+        const { channelId, userId } = peerId
+        const chatid = channelId?.toString() ?? userId.toString()
         if(message.media)
         {
             if (!fs.existsSync(downloadsDirectory))
                 fs.mkdirSync(downloadsDirectory);
-            const filePath = `${downloadsDirectory}/${channelId ?? userId}-${id}-${message.media.document.attributes[0].fileName}`
+            const filePath = `${downloadsDirectory}/${chatid}-${id}-${message.media.document.attributes[0].fileName}`
+            console.log(`downloading to ${filePath}`)
             await message.downloadMedia({
                 outputFile: filePath
             })
-            const data = await parseFile(filePath);
+            console.log(`downloading done`)
+            console.log(`parsing ${filePath}`)
+            const data = await parseFile("./.downloads/dummy.txt");
+            console.log(`parsing done`)
             console.log(data)
+            
+            if(data){
+                console.log(`inserting ${data.length} credentials`)
+                await insertCredentials(data,chatid,id)
+                console.log(`insert done`)
+            }
+
+            fs.unlink(filePath, (err) => {
+                if (err) throw err;
+                console.log(`${filePath} was deleted`);
+            });
         }
     }
 };
