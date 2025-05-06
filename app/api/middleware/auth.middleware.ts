7a3e6f555acf6ecee88ddb96903ef143656238c5
Max <maxjapri@gmail.com>
2025-05-06 18:03:40 +0700

    user checking on middleware

--- app/api/middleware/auth.middleware.ts
+++ app/api/middleware/auth.middleware.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import httpStatus from 'http-status';
 import { CustomError } from './error-handler.middleware'
 import config from '../constraints/config';
+import { getUserByEmail } from '../helper/users.helper';
 
 export default async function auth(req: Request, res: Response, next: NextFunction) {
     try
@@ -15,6 +16,8 @@ export default async function auth(req: Request, res: Response, next: NextFuncti
         const decodedPayload = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString('utf8'));
         jwt.verify(token, config.JWT_SECRET_KEY);
         res.locals.user = decodedPayload;
+        const checkUser = await getUserByEmail(res.locals.user.email)
+        if(!checkUser) next(new CustomError('User not registered',httpStatus.UNAUTHORIZED));
         next();
     }
     catch(error: any)
