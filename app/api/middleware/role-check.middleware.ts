2d1ef1d198dad7fc81e547f0ea781ee0381f6ce5
Max <maxjapri@gmail.com>
2025-05-07 22:39:58 +0700

    rename middleware rolechecking roles to role

--- app/api/middleware/role-check.middleware.ts
+++ app/api/middleware/role-check.middleware.ts
@@ -4,7 +4,7 @@ import status from 'http-status';
 
 export default function rolecheck(roles: Number[]) {
     return function (req: Request, res: Response, next: NextFunction) {
-        const userrole = res.locals.user.roles as Number
+        const userrole = res.locals.user.role as Number
         if(!roles.includes(userrole))
             throw new CustomError("User Role Unauthorized", status.UNAUTHORIZED)
         else next();
